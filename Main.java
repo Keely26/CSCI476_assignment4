462fbac4ed2a85f22457baa160915e59db7869b2
Keely Weisbeck <keelyweisbeck@gmail.com>
2019-03-26 11:47:43 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,64 @@
+package com.javahelps.pcapparser;
+
+import io.pkts.PacketHandler;
+import io.pkts.Pcap;
+import io.pkts.packet.Packet;
+import io.pkts.packet.TCPPacket;
+import io.pkts.packet.UDPPacket;
+import io.pkts.protocol.Protocol;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+
+
+
+    public static void main(String[] args) throws IOException {
+
+        final Pcap pcap = Pcap.openStream("lbl-internal.20041004-1305.port002.dump.pcap");
+        final Map<String, Integer> SYN_count = new HashMap<>();
+        final Map<String, Integer> SYNACK_count = new HashMap<>();
+
+        pcap.loop(new PacketHandler() {
+            @Override
+            public boolean nextPacket(Packet packet) throws IOException {
+                //List<Packet> SYN = new ArrayList<>();
+                //List<Packet> SYNACK = new ArrayList<>();
+                Map<Packet, List> SYN = new HashMap<>();
+                Map<Packet, List> SYNACK = new HashMap<>();
+                if (packet.hasProtocol(Protocol.TCP)) {
+                    TCPPacket tcpPacket = (TCPPacket) packet.getPacket(Protocol.TCP);
+                    if (tcpPacket.isSYN() && !tcpPacket.isACK()){ // SYN
+//                        List<String> IPs = new ArrayList<>();
+////                        IPs.add(tcpPacket.getDestinationIP());
+////                        IPs.add(tcpPacket.getSourceIP());
+////                        SYN.put(tcpPacket, IPs);
+                        if(SYN_count.containsKey(tcpPacket.getSourceIP())){
+                            SYN_count.put(tcpPacket.getSourceIP(), SYN_count.get(tcpPacket.getSourceIP()) + 1);
+                        }
+                    }
+                    if (tcpPacket.isACK() && tcpPacket.isSYN()){ //SYN ACK
+                        List<String> IPs = new ArrayList<>();
+                        IPs.add(tcpPacket.getDestinationIP());
+                        IPs.add(tcpPacket.getSourceIP());
+                        SYNACK.put(tcpPacket, IPs);
+                    }
+
+                    for (Map.Entry<Packet,List> entry : SYN.entrySet())
+                        System.out.print("Key = " + entry.getKey().getName() +
+                                " Value = " + entry.getValue());
+
+                } else if (packet.hasProtocol(Protocol.UDP)) {
+                    UDPPacket udpPacket = (UDPPacket) packet.getPacket(Protocol.UDP);
+                }
+                return true;
+            }
+
+        });
+    }
+
+}
